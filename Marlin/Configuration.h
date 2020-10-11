40d326967b9f57eeb91225f973db861ec1e1671f
mchlbatista <maikelbatistacruz@gmail.com>
2020-10-11 18:52:22 -0500

    Homing feedrate for XY set to 25*60 to prevent hard crash against the endstop

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1387,7 +1387,7 @@
 #endif
 
 // Homing speeds (mm/min)
-#define HOMING_FEEDRATE_XY (50*60)
+#define HOMING_FEEDRATE_XY (25*60)
 #define HOMING_FEEDRATE_Z  (4*60)
 
 // Validate that endstops are triggered on homing moves
